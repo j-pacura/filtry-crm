725eb6f5444ab671efd93a3ed9745d15705d9ec0
j-pacura <pacura.jakub@gmail.com>
2025-08-17 17:42:38 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,20 @@ import './index.css'
 import 'leaflet/dist/leaflet.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
+import Login from './pages/Login'
+import AuthGate from './components/AuthGate'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route element={<AuthGate />}>
+          <Route path="/*" element={<App />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 )
